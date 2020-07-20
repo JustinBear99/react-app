27e260428a0980a959fdc97894b8b06204b959b8
JustinBear <justin850509@gmail.com>
2020-07-20 20:28:31 +0800

    backup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Asparagus from './components/Asparagus';
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Asparagus />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Asparagus.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Asparagus extends React.Component{
+    render(){
+        return(
+            <div className="Asparagus">
+                <h1>Monitoring system</h1>
+                <div>
+                    <form>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Asparagus;
